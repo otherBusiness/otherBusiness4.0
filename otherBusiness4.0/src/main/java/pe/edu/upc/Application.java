b333e4cd2f9189072af66b923c47fb7665a19615
Mario <Mario@Lenovo-PC>
2020-05-20 03:15:45 -0500

    Error de compilacion solucionado y creado una bd: otherbusinessdb

--- otherBusiness4.0/src/main/java/pe/edu/upc/Application.java
+++ otherBusiness4.0/src/main/java/pe/edu/upc/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// No olvidar que el nombre de la clase es Application
 public class Application {
 
 	public static void main(String[] args) {
